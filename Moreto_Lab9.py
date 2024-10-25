e5aa205af9afb64cf51ee6e7bfcc874d7ce946e5
Keith Moreto <moretokeithmichael@gmail.com>
2024-10-25 14:46:11 +0800

    Moreto_Lab9.py

--- Moreto_Lab9.py
+++ Moreto_Lab9.py
@@ -1,10 +1,18 @@
 #print the number of rows for the right-angled triangle
 num_rows =int(input('Enter The number of rows: '))
+
 current_number = 1 #start from 1
+triangle_row = ''
 
 
-for row in range(1, num_rows + 1):
-    for col in range (1, row + 1):
-        print(col, end=' ')
-        current_number += 1
-    print()
+for row in range(1, num_rows + 1): #rows
+
+    for column in range(row): #columns
+
+      triangle_row += str(current_number) + ' ' # Adds columns
+
+      current_number += 1 # Adds value to next number
+
+    print(triangle_row) # Displays a row
+
+    triangle_row = '' #row reset
